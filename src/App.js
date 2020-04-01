ab61013c403c217287750ba879e579ca4941f7d3
Vikram Durai <vikramvdurai@gmail.com>
2020-04-01 23:31:26 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+//import { createStore } from 'redux';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Todo extends React.Component {
+	render() {
+		return (
+			<div id={this.props.id}>
+				<span><p>{this.props._todo}</p><button onClick={this.props.tRemove}>Remove</button></span>
+			</div>
+		);
+	}
+}
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.updateTodos = this.updateTodos.bind(this);
+		this.removeTodos = this.removeTodos.bind(this);
+		//this.store = createStore(this.updateTodos);
+		this.state = {
+			todosList: []
+		};
+	}
+	updateTodos() {
+			var self = this;
+			function createTodo(tmsg) {
+				return (
+						<Todo id={tmsg.value} key={tmsg.value} tRemove={self.removeTodos(tmsg.value)} _todo={tmsg.value} />
+				);
+			}
+			let tmsg = document.getElementById("tMsg");
+			if (!tmsg || !tmsg.value) { return; }
+			this.setState({todosList: this.state.todosList.concat(createTodo(tmsg))});
+			//this.forceUpdate();
+			console.log("yippee");
+			console.log("shut up vikram", this.state.todosList);
+	}
+	removeTodos(todoId) {
+		return function() {
+				let todo = document.getElementById(todoId);
+				if (!todo || !todo.innerText) { return; } // ghost todo?
+				todo.remove();
+		}
+	}
+	render() {
+		return (
+			<div>
+				<input id="tMsg" placeholder="Write a todo!"></input>
+				<button onClick={this.updateTodos}>Create New Todo</button>
+				{this.state.todosList}
+			</div>
+		);
+	}
 }
 
 export default App;
